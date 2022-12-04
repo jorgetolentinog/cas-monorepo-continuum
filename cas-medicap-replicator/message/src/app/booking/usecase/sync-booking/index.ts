840fd417f3d5f637625f85ec7c204862a7ba5839
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-12-03 20:17:59 -0500

    -

--- cas-medicap-replicator/message/src/app/booking/usecase/sync-booking/index.ts
+++ cas-medicap-replicator/message/src/app/booking/usecase/sync-booking/index.ts
@@ -1,5 +1,10 @@
+import { metrics } from "@package/metrics";
+import { eventbus } from "@package/eventbus";
 import { SyncBooking } from "./sync-booking";
 import { bookingRepository } from "../../repository";
-import { metrics } from "@package/metrics";
 
-export const syncBooking = new SyncBooking(bookingRepository, metrics);
+export const syncBooking = new SyncBooking(
+  bookingRepository,
+  eventbus,
+  metrics
+);
